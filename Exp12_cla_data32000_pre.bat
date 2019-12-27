815add71eb03e66395df4573f7f9bd754bcc8fb9
LeoWood <463861936@qq.com>
2019-12-27 20:00:44 +0800

    .

--- Exp12_cla_data32000_pre.bat
+++ Exp12_cla_data32000_pre.bat
@@ -4,14 +4,14 @@ python run_classifier.py^
  --bert_config_file models/chinese_L-12_H-768_A-12/bert_config.json^
  --task_name cla^
  --vocab_file models/chinese_L-12_H-768_A-12/vocab.txt^
- --output_dir outputs/Exp12_cla_data32000_pre^
- --init_checkpoint outputs/Pre2_cscd_R_128_from_bert/model.ckpt-250000^
+ --output_dir outputs/Eval_data32000_pre_from_bert_80w^
+ --init_checkpoint outputs/Pre2_cscd_R_128_from_bert/model.ckpt-800000^
  --max_seq_length 400^
  --do_train True^
  --do_predict True^
  --train_batch_size 8^
  --learning_rate 2e-5^
  --num_train_epochs 2.0^
- --gpu 1^
+ --gpu 0^
  --cla_nums 16
 
