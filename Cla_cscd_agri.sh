9c1549b001e5bf8da07ff6fbfea2f23d5b0f7983
LeoWood <463861936@qq.com>
2020-04-15 10:58:05 +0800

    .

--- Cla_cscd_agri.sh
+++ Cla_cscd_agri.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 python run_classifier.py\
- --data_dir=data/data_cla_cscd_tp\
+ --data_dir=data/data_cla_cscd_agri\
  --bert_config_file=models/chinese_L-12_H-768_A-12/bert_config.json\
  --task_name=cla\
  --vocab_file=models/chinese_L-12_H-768_A-12/vocab.txt\
- --output_dir=outputs/Cla_cscd_tp_base\
+ --output_dir=outputs/Cla_cscd_agri_base\
  --init_checkpoint=models/chinese_L-12_H-768_A-12/bert_model.ckpt\
  --max_seq_length=400\
  --do_train=True\
  --do_predict=True\
  --train_batch_size=8\
  --learning_rate=2e-5\
- --num_train_epochs=3.0\
- --gpu=0\
- --cla_nums=43
+ --num_train_epochs=2.0\
+ --gpu=1\
+ --cla_nums=52
