b94678c64787fb7b7786827de89b0cb9fc87285f
LeoWood <463861936@qq.com>
2020-06-30 18:30:42 +0800

    .

--- work/create_pre_data/process_pre_data.py
+++ work/create_pre_data/process_pre_data.py
@@ -6,6 +6,7 @@ import re
 from pySql import pySql
 from Seg_Sents_Cn import Seg_Sents_Cn
 import json
+from tqdm import tqdm
 
 #数据库连接
 # 读取数据库信息
@@ -25,7 +26,7 @@ def ReadDb_SegmentSentence_Export2Txt(label2text):
             sql = "SELECT id,abstract FROM article_info where classification like '{cla_str}%' and language='chi' order by id".format(
                 cla_str=label)
             df = db_server.read_sql(sql)
-            for i in range(len(df)):
+            for i in tqdm(range(len(df))):
                 abstract = df.iloc[i]['abstract']
                 SentencesList = Seg_Sents_Cn(abstract)
                 for sen in SentencesList:
@@ -36,6 +37,26 @@ def ReadDb_SegmentSentence_Export2Txt(label2text):
                 if count % 100 == 0:
                     print(count,' Done.')
         print(label , ' Done')
+    print('句子总数:', count)
+
+def write_to_single_file(label2text):
+    with open(label2text, 'r', encoding='utf-8') as f:
+        cla_dict = json.load(f)
+    count = 0
+    with open('CSCD.txt', 'w', encoding='utf-8') as f:
+        for label, text in cla_dict.items():
+            sql = "SELECT id,abstract FROM article_info where classification like '{cla_str}%' and language='chi' order by id".format(
+                cla_str=label)
+            df = db_server.read_sql(sql)
+            for i in tqdm(range(len(df))):
+                abstract = df.iloc[i]['abstract']
+                SentencesList = Seg_Sents_Cn(abstract)
+                for sen in SentencesList:
+                    f.write(sen + '\n')
+                    count += 1
+                f.write('\n')
+
+        print(label, ' Done')
     print('句子总数:', count)
 
 def getsomememory(label2text):
@@ -63,7 +84,7 @@ def getsomememory(label2text):
 
 
 if __name__ == '__main__':
-    ReadDb_SegmentSentence_Export2Txt(r'R.json')
+    write_to_single_file(r'cla_1_label2text_filter.json')
     # ReadDb_SegmentSentence_Export2Txt(r'cla_1_label2text_filter.json')
  #    with open(r'cla_1_label2text_filter.json', 'r', encoding='utf-8') as f:
  #        cla_dict = json.load(f)
