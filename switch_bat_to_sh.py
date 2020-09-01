a831f89ebd17f12a553158c98e0a6c6aa66a3008
LeoWood <463861936@qq.com>
2020-09-01 16:42:05 +0800

    .

---
+++ switch_bat_to_sh.py
@@ -0,0 +1,22 @@
+# -*- encoding:utf-8 -*-
+import re
+import os
+import sys
+os.chdir(sys.path[0])
+
+def switch(bat_f,sh_f):
+    with open(bat_f,'r',encoding='utf-8') as f:
+        commond = f.read()
+
+    commond = commond.replace('set ','export ')
+    commond = commond.replace('^','\\')
+    vars = re.findall('%.[^%]*%',commond)
+    switch_vars = [v.replace('%','$')[:-1] for v in vars]
+    for var,switch_var in zip(vars,switch_vars):
+        commond = commond.replace(var,switch_var)
+    print(commond)
+    with open(sh_f,'w',encoding='utf-8') as f:
+        f.write(commond)
+
+if __name__ == '__main__':
+    switch('Cla_yu copy.bat','cla_shanghai.sh')
